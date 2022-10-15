b261a7beaf460f7e8eb141cc61a35321ca2c260c
Manash Pratim Kalita <manashpk05@gmail.com>
2022-10-15 21:30:06 +0530

    Comments added

--- main.c
+++ main.c
@@ -1,18 +1,24 @@
+// C code for ARITHMETIC CALCULATOR
+
 #include<stdio.h>
 #include<unistd.h>
 
+// Function to add
 double add(double number1, double number2){
     return number1 + number2;
 }
 
+// Function to substract
 double substract(double number1, double number2){
     return number1 - number2;
 }
 
+// Function to multiply
 double multiply(double number1, double number2){
     return number1 * number2;
 }
 
+// Function to divide
 double divide(double number1, double number2){
     return number1 / number2;
 }
@@ -34,6 +40,7 @@ int main(){
         printf("Please enter your choice: ");
         scanf("%d", &choice);
 
+        // Taking user input for the operations
         printf("Enter the first number: ");
         scanf("%lf", &number1);
 
@@ -42,26 +49,31 @@ int main(){
 
 
         if (choice == 1){
+            // Addition
             printf("\t%.2lf + %.2lf = %.2lf\n", number1, number2, add(number1, number2));
         }  
 
         else if (choice == 2){
+            // Substraction
             printf("\t%.2lf - %.2lf = %.2lf\n", number1, number2, substract(number1, number2));
         }  
 
         else if (choice == 3){
+            // Multiplication
             printf("\t%.2lf * %.2lf = %.2lf\n", number1, number2, multiply(number1, number2));
         }
 
         else if (choice == 4){
+            // Division
             printf("\t%.2lf / %.2lf = %.2lf\n", number1, number2, divide(number1, number2));
         }    
+        // ".2lf" ---> 2 digits after the decimal point
 
         else{
             printf("Wrong Input.\n");
         }
 
-        sleep(1);
+        sleep(1);       // Program sleeps for 1 second
 
         printf("Enter '1' to continue.\n"
                 "Enter '2' to exit. ");
